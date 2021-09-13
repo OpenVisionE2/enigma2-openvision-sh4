be0ae1751479146a812a8bacc89a2c4d4a82985c
Jason Hood <jadoxa@yahoo.com.au>
2021-09-13 20:54:17 +1000

    Fix marked text offset
    The offset for marked text was stored in the context.  This caused an
    incorrect offset for screens with multiple labels.  In particular, the
    virtual keyboard input line could shift after selecting another key.
    Store the offset directly in eLabel & eListboxPythonMultiContent and
    pass it as a parameter to renderText.  As a consequence of this change,
    the space used to indicate the end of a config string has become a
    non-breaking space (as space is removed by right alignment).

--- lib/gdi/grc.h
+++ lib/gdi/grc.h
@@ -88,6 +88,7 @@ struct gOpcode
 			gRGB bordercolor;
 			int markedpos;
 			int scrollpos;
+			int *offset;
 		} *renderText;
 
 		struct prenderPara
@@ -234,7 +235,7 @@ public:
 
 		RT_WRAP = 64
 	};
-	void renderText(const eRect &position, const std::string &string, int flags=0, gRGB bordercolor=gRGB(), int border=0, int markedpos=-1);
+	void renderText(const eRect &position, const std::string &string, int flags=0, gRGB bordercolor=gRGB(), int border=0, int markedpos=-1, int *offset=0);
 
 	void renderPara(eTextPara *para, ePoint offset=ePoint(0, 0));
 
@@ -292,7 +293,6 @@ protected:
 	gRGB m_foreground_color_rgb, m_background_color_rgb;
 	ePtr<gFont> m_current_font;
 	ePoint m_current_offset;
-	int m_text_offset;
 
 	std::stack<gRegion> m_clip_stack;
 	gRegion m_current_clip;

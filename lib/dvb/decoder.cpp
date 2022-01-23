3d8ff6a8b0162a5a2b9181c40cf8d2cdd67d2628
nautilus7 <spyros.n7@gmail.com>
2022-01-23 04:28:40 +0200

    ConfigMenu.py: Delete broken and unused module
    This module contains 2 classes that are completely broken and not used anywhere in enigma2.

--- lib/dvb/decoder.cpp
+++ lib/dvb/decoder.cpp
@@ -316,7 +316,7 @@ eDVBVideo::eDVBVideo(eDVBDemux *demux, int dev)
 #define VIDEO_STREAMTYPE_MPEG1 6
 #define VIDEO_STREAMTYPE_H265_HEVC 7
 #define VIDEO_STREAMTYPE_AVS 16
-#define VIDEO_STREAMTYPE_AVS2 17
+#define VIDEO_STREAMTYPE_AVS2 40
 
 int eDVBVideo::startPid(int pid, int type)
 {

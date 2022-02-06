cccab259811b3d280bddeea812a12d5bdd6d0684
IanSav <IanSav@users.noreply.github.com>
2022-02-06 21:49:07 +1100

    [config.py] Remove unused and broken method

--- lib/python/Components/config.py
+++ lib/python/Components/config.py
@@ -658,9 +658,6 @@ class ConfigDictionarySet(ConfigElement):
 			if callable(self.callback):
 				self.callback()
 
-	def getKeys(self):
-		return self.dir_pathes
-
 
 # This is the control, and base class, for location settings.
 #

89d32f2322484e8a8ff7d8c97d10437a395de74c
norhap <elfogondesegovia@gmail.com>
2022-02-26 03:35:45 +0100

    StartEnigma.py: fix typo add operators

--- lib/python/StartEnigma.py
+++ lib/python/StartEnigma.py
@@ -355,7 +355,7 @@ def runScreen():
 		setFPWakeuptime(wakeupTime)
 	if powerTimerList and powerTimerList[0][1] == 3:
 		startTimePowerList = powerTimerList[0]
-		if (startTimePowerList[0], nowTime) < 60:  # No time to switch box back on.
+		if (startTimePowerList[0] - nowTime) < 60:  # No time to switch box back on.
 			wakeupTime = nowTime + 30  # So switch back on in 30 seconds.
 		else:
 			wakeupTime = startTimePowerList[0]
@@ -367,7 +367,7 @@ def runScreen():
 	config.misc.isNextPowerTimerAfterEventActionAuto.save()
 	if powerTimerList and powerTimerList[0][1] != 3:
 		startTimePowerList = powerTimerList[0]
-		if (startTimePowerList[0], nowTime) < 270:  # No time to switch box back on.
+		if (startTimePowerList[0] - nowTime) < 270:  # No time to switch box back on.
 			wakeupTime = nowTime + 30  # So switch back on in 30 seconds.
 		else:
 			wakeupTime = (startTimePowerList[0], 240)

0289b6d888855d45436ad0d19407eb43e17d50e9
norhap <elfogondesegovia@gmail.com>
2022-08-11 09:20:30 +0200

    PowerTimer.py: Log - Add button blue enable / disable Log
    - pm_timers.xml increases its data quickly with self.logs, if the user restores copies of the file  it will grow gradually.
    
    Then it will be the user's decision to activate or not the registration.
    
    - By default, this XML is left with only a first line of execution, that is, powertimerlog disabled, unless the user decides to augment it with data to see the logs with the blue button.
    
    - Log enabled or disabled does not affect the work of feed timers at all.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1677,6 +1677,8 @@ def InitUsageConfig():
 	config.misc.softcam_setup.extension_menu = ConfigYesNo(default=True)
 
 	config.ntp = ConfigSubsection()
+	config.powertimerlog = ConfigSubsection()
+	config.powertimerlog.actived = ConfigYesNo(default=False)
 
 	def timesyncChanged(configElement):
 		if configElement.value == "dvb" or not GetIPsFromNetworkInterfaces():

5ea966408576e23498ae5f9d28a438713249caed
norhap <elfogondesegovia@gmail.com>
2022-10-02 09:28:42 +0200

    Information.py: add subtype proc
    - there are machines with subtype and not type. e.g. osmio4kplus.
    
    Hardware type: show number hardward.
    Tuner type: show type tuner.

--- lib/python/Screens/Information.py
+++ lib/python/Screens/Information.py
@@ -64,7 +64,7 @@ def getBoxProcTypeName():
 	procType = getBoxProcType()
 	if procType == "unknown":
 		return _("Unknown")
-	return "%s  -  %s" % (procType, boxProcTypes.get(procType, _("Unknown")))
+	return "%s - %s" % (procType, boxProcTypes.get(procType, _("Unknown")))
 
 
 class InformationBase(Screen, HelpableScreen):
@@ -1099,9 +1099,7 @@ class ReceiverInformation(InformationBase):
 		adbVariant = fileReadLine("/proc/stb/info/adb_variant", source=MODULE_NAME)
 		if adbVariant:
 			info.append(formatLine("P1", _("ADB variant"), adbVariant))
-		procModelType = getBoxProcTypeName()
-		if procModelType and procModelType != "unknown":
-			info.append(formatLine("P1", _("Hardware type"), procModelType))
+		info.append(formatLine("P1", _("Hardware type"), getBoxProcTypeName().split("-")[0])) if getBoxProcTypeName() != _("unknown") else ""
 		hwSerial = getHWSerial()
 		if hwSerial:
 			info.append(formatLine("P1", _("Hardware serial"), (hwSerial if hwSerial != "unknown" else about.getCPUSerial())))
@@ -1539,6 +1537,7 @@ class TunerInformation(InformationBase):
 			info.append(formatLine("P1", "Tuner %s:" % data))
 			data = descList[count]["start"] if descList[count]["start"] == descList[count]["end"] else ("%s-%s" % (descList[count]["start"], descList[count]["end"]))
 			info.append(formatLine("P2", "%s" % descList[count]["desc"]))
+		info.append(formatLine("P1", _("Tuner type"), "%s" % getBoxProcTypeName().split("-")[1])) if getBoxProcTypeName() != _("Unknown") else ""
 		# info.append("")
 		# info.append(formatLine("H", _("Logical tuners")))  # Each tuner is a listed separately even if the hardware is common.
 		# info.append("")

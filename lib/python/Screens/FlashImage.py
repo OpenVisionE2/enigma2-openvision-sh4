norhap/enigma2

FlashImage.py: improve directory check before delete

lib/python/Screens/FlashImage.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from os import access, listdir, major, minor, mkdir, remove, rmdir, sep, stat, statvfs, walk, W_OK
-from os.path import isdir, isfile, join, splitext, islink, ismount
+from os.path import isdir, isfile, join, splitext, islink, ismount, exists
 from json import load
 from zipfile import ZipFile
 from shutil import copyfile, rmtree
@@ -448,7 +448,7 @@ class MultibootSelection(SelectImage):
 
 	def cancel(self, value=None):
 		Console().ePopen('umount %s' % self.tmp_dir)
-		if not ismount(self.tmp_dir):
+		if exists(self.tmp_dir):
 			rmdir(self.tmp_dir)
 		if value == 2 and not isfile(join(self.tmp_dir, "STARTUP")):
 			self.session.open(TryQuitMainloop, 2)

OpenVisionE2/enigma2-openvision-sh4

lib/python/Screens/FlashImage.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from json import load
 from os import access, listdir, major, minor, mkdir, remove, rmdir, sep, stat, statvfs, walk, W_OK
-from os.path import isdir, isfile, join, splitext, ismount, islink
+from os.path import isdir, isfile, join, splitext, ismount, islink, exists
 from shutil import copyfile, rmtree
 from tempfile import mkdtemp
 from struct import pack
@@ -449,7 +449,7 @@ class MultiBootSelection(SelectImage):
 
 	def cancel(self, value=None):
 		Console().ePopen('umount %s' % self.tmp_dir)
-		if not ismount(self.tmp_dir):
+		if exists(self.tmp_dir):
 			rmdir(self.tmp_dir)
 		if value == 2 and not isfile(join(self.tmp_dir, "STARTUP")):
 			self.session.open(TryQuitMainloop, 2)
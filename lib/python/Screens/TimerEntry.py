cc01a4fb76471ae8125fcf153e82c8c2a14e68b0
norhap <elfogondesegovia@gmail.com>
2023-02-14 00:00:23 +0100

    TimerEntry.py: define MENU key show keyMenuCallback values

--- lib/python/Screens/TimerEntry.py
+++ lib/python/Screens/TimerEntry.py
@@ -48,6 +48,7 @@ class TimerEntry(ConfigListScreen, Screen):
 			"ok": self.keySelect,
 			"save": self.keyGo,
 			"cancel": self.keyCancel,
+			"menu": self.keyMenu,
 			"volumeUp": self.incrementStart,
 			"volumeDown": self.decrementStart,
 			"size+": self.incrementEnd,
@@ -548,6 +549,9 @@ class TimerEntry(ConfigListScreen, Screen):
 
 	def keyCancel(self):
 		self.close((False,))
+
+	def keyMenu(self):
+		ConfigListScreen.keyMenu(self)
 
 	def pathSelected(self, res):
 		if res is not None:

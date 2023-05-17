16cc9b9ead96b47f794d55a48b8a65024774b345
persianpros <persianpros@yahoo.com>
2023-05-17 08:09:40 +0330

    MovieInfo.py: Movie Name displayed when no EIT file is available
    Co-authored-by: jbleyel <jbleyel@gmx.net>

--- lib/python/Components/Converter/MovieInfo.py
+++ lib/python/Components/Converter/MovieInfo.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+from os.path import basename, normpath
 from Components.Converter.Converter import Converter
 from Components.Element import cached, ElementError
 from enigma import iServiceInformation, eServiceReference
 from ServiceReference import ServiceReference
+from Components.UsageConfig import dropEPGNewLines, replaceEPGSeparator
+from Components.config import config
 
 
 class MovieInfo(Converter):
@@ -10,6 +13,8 @@ class MovieInfo(Converter):
 	MOVIE_META_DESCRIPTION = 1 # just meta description when available
 	MOVIE_REC_SERVICE_NAME = 2 # name of recording service
 	MOVIE_REC_FILESIZE = 3 # filesize of recording
+	MOVIE_FULL_DESCRIPTION = 4 # short and exended description
+	MOVIE_NAME = 5 # recording name
 
 	def __init__(self, type):
 		if type == "ShortDescription":
@@ -20,6 +25,10 @@ class MovieInfo(Converter):
 			self.type = self.MOVIE_REC_SERVICE_NAME
 		elif type == "FileSize":
 			self.type = self.MOVIE_REC_FILESIZE
+		elif type == "FullDescription":
+			self.type = self.MOVIE_FULL_DESCRIPTION
+		elif type == "Name":
+			self.type = self.MOVIE_NAME
 		else:
 			raise ElementError("'%s' is not <ShortDescription|MetaDescription|RecordServiceName|FileSize> for MovieInfo converter" % type)
 		Converter.__init__(self, type)
@@ -30,22 +39,38 @@ class MovieInfo(Converter):
 		info = self.source.info
 		event = self.source.event
 		if info and service:
-			if self.type is self.MOVIE_SHORT_DESCRIPTION:
-				if (service.flags & eServiceReference.flagDirectory) == eServiceReference.flagDirectory:
+			isDirectory = (service.flags & eServiceReference.flagDirectory) == eServiceReference.flagDirectory
+			if self.type == self.MOVIE_SHORT_DESCRIPTION:
+				if isDirectory:
 					# Short description for Directory is the full path
 					return service.getPath()
 				return (info.getInfoString(service, iServiceInformation.sDescription)
-					or (event and event.getShortDescription())
+					or (event and dropEPGNewLines(event.getShortDescription()))
 					or service.getPath())
-			elif self.type is self.MOVIE_META_DESCRIPTION:
-				return ((event and (event.getExtendedDescription() or event.getShortDescription()))
+			elif self.type == self.MOVIE_FULL_DESCRIPTION:
+				if isDirectory:
+					return ""
+				description = (event and dropEPGNewLines(event.getShortDescription())
+						or info.getInfoString(service, iServiceInformation.sDescription))
+				extended = event and dropEPGNewLines(event.getExtendedDescription().rstrip())
+				if description and extended:
+					if description.replace('\n', '') == extended.replace('\n', ''):
+						return extended
+					description += replaceEPGSeparator(config.epg.fulldescription_separator.value)
+				return description + (extended if extended else "")
+			elif self.type == self.MOVIE_META_DESCRIPTION:
+				return ((event and (dropEPGNewLines(event.getExtendedDescription()) or dropEPGNewLines(event.getShortDescription())))
 					or info.getInfoString(service, iServiceInformation.sDescription)
 					or service.getPath())
-			elif self.type is self.MOVIE_REC_SERVICE_NAME:
+			elif self.type == self.MOVIE_NAME:
+				if isDirectory:
+					return basename(normpath(service.getPath()))
+				return event and event.getEventName() or info and info.getName(service)
+			elif self.type == self.MOVIE_REC_SERVICE_NAME:
 				rec_ref_str = info.getInfoString(service, iServiceInformation.sServiceref)
 				return ServiceReference(rec_ref_str).getServiceName()
-			elif self.type is self.MOVIE_REC_FILESIZE:
-				if (service.flags & eServiceReference.flagDirectory) == eServiceReference.flagDirectory:
+			elif self.type == self.MOVIE_REC_FILESIZE:
+				if isDirectory:
 					return _("Directory")
 				filesize = info.getFileSize(service)
 				if filesize is not None:
